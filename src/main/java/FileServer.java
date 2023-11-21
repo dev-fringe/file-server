26ef229c322f9b683ba23c07a9505f968c37a963
hdlee <dev.fringe@gmail.com>
2023-11-21 17:58:00 +0900

    init

--- src/main/java/FileServer.java
+++ src/main/java/FileServer.java
@@ -1,17 +1,44 @@
+import static io.undertow.Handlers.resource;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.nio.file.Paths;
+import java.util.Enumeration;
 
 import io.undertow.Undertow;
 import io.undertow.server.handlers.resource.PathResourceManager;
-import static io.undertow.Handlers.resource;
 public class FileServer {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SocketException {
         Undertow
         .builder()
-        .addHttpListener(8080, "0.0.0.0")
+        .addHttpListener(8080, getLocalIP())
         .setHandler(
         		resource(new PathResourceManager(Paths.get(System.getProperty("user.home")), 100)).setDirectoryListingEnabled(true))
         .build()
         .start();
 	}
+	
+	
+    public static String getLocalIP() throws SocketException {
+        String localIP = null;
+        Enumeration allNetInterfaces = NetworkInterface.getNetworkInterfaces();
+        InetAddress ip = null;
+        while (allNetInterfaces.hasMoreElements()) {
+            NetworkInterface netInterface = (NetworkInterface) allNetInterfaces.nextElement();
+            Enumeration addresses = netInterface.getInetAddresses();
+            while (addresses.hasMoreElements()) {
+                ip = (InetAddress) addresses.nextElement();
+                if (ip != null && ip instanceof Inet4Address) {
+                    localIP = ip.getHostAddress();
+                    if (!"127.0.0.1".equalsIgnoreCase(localIP)) {
+                        return localIP;
+                    }
+                }
+            }
+        }
+        return localIP;
+    }
 }
